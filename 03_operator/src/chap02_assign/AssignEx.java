1b01849f320bea0c7be11530c24f9ad15accbc20
sss137 <tlstjdtn137@naver.com>
2025-06-26 17:21:22 +0900

    06.26

---
+++ 03_operator/src/chap02_assign/AssignEx.java
@@ -0,0 +1,17 @@
+package chap02_assign;
+
+public class AssignEx {
+
+  public static void main(String[] args) {
+    //Assignment: 대입 연산(=)
+    
+    //Compound Assignment: 복합 대입 연산(+=, -=, *=, /=, %= 등)
+    int x = 10;
+    x += 10; System.out.println(x);
+    x -= 10; System.out.println(x);
+    x *= 10; System.out.println(x);
+    x /= 10; System.out.println(x);
+    x %= 10; System.out.println(x);
+  }
+
+}
